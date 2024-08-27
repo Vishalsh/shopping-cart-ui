7c2da5f6e3eb2669459b88edb94828a08fdaa47e
Vishal Sharma <vishalsharma@Vishals-MacBook-Pro-2.local>
2024-08-27 12:05:34 +0530

    ui for learnig devops

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import {useEffect, useState} from 'react'
+
+function App() {
+    const [products, setProducts] = useState([])
+
+    async function getProducts() {
+        const response = await fetch('http://localhost:3000/products')
+        const products = await response.json()
+        setProducts(products);
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    return (
+        <>
+            {
+                <ul>
+                    {
+                        products.map((product, index) => (
+                            <li key={index}>
+                                {product.name}: {product.price}
+                            </li>
+                        ))
+                    }
+                </ul>
+            }
+        </>
+    )
+}
+
+export default App
